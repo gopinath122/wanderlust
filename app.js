78dd3d60dae4cdbb90b54b6f09babac250e93245
Gopinath <gopinathghevade3@gmail.com>
2025-11-30 01:32:43 +0530

    Ready for production deployment

--- app.js
+++ app.js
@@ -85,13 +85,14 @@ store.on("error", (err) => {
 
 const sessionOptions = {
   store: store,
-  secret: process.env.SECRET,
+  secret: process.env.SECRET || "mysecretcode",
   resave: false,
   saveUninitialized: false,
   cookie: {
     expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
     maxAge: 7 * 24 * 60 * 60 * 1000,
     httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
   },
 };
 app.use(session(sessionOptions));
